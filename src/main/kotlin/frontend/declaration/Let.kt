e0a412d4d2dfc16a03963932fdc54636f2915246
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2020-11-15 18:42:00 -0500

    Detect infinitely recursive let value initialization

--- src/main/kotlin/frontend/declaration/Let.kt
+++ src/main/kotlin/frontend/declaration/Let.kt
@@ -6,7 +6,9 @@ import xyz.qwewqa.trebla.frontend.context.Signature
 import xyz.qwewqa.trebla.frontend.context.Visibility
 import xyz.qwewqa.trebla.frontend.context.visibilityModifiers
 import xyz.qwewqa.trebla.frontend.expression.UnitValue
+import xyz.qwewqa.trebla.frontend.expression.Value
 import xyz.qwewqa.trebla.frontend.expression.parseAndApplyTo
+import xyz.qwewqa.trebla.frontend.runWithErrorMessage
 import xyz.qwewqa.trebla.grammar.trebla.LetDeclarationNode
 
 class LetDeclaration(
@@ -37,14 +39,34 @@ class LetDeclaration(
             compileError("All underscore identifiers are reserved.")
         }
         context.scope.addLazy(
-            lazy {
-                val value = node.expression.parseAndApplyTo(context)
-                value.coerceTo(typeConstraint)
-                    ?: compileError("Value of type ${value.type.commonName} does not satisfy explicit type ${typeConstraint.commonName}.")
+            LetLazyValue {
+                node.expression.runWithErrorMessage("Error in let declaration.") {
+                    val value = node.expression.parseAndApplyTo(context)
+                    value.coerceTo(typeConstraint)
+                        ?: compileError("Value of type ${value.type.commonName} does not satisfy explicit type ${typeConstraint.commonName}.")
+                }
             },
             identifier, signature, visibility,
         )
         return UnitValue
     }
 
+    inner class LetLazyValue(val initializer: () -> Value) : Lazy<Value> {
+        private var initializing = false
+        private var initialized = false
+        private var _value: Value? = null
+
+        override val value: Value
+            get() {
+                if (initialized) return _value!!
+                if (initializing) compileError("Let value accessed while still initializing.")
+                initializing = true
+                _value = initializer()
+                initializing = false
+                initialized = true
+                return _value!!
+            }
+
+        override fun isInitialized() = initialized
+    }
 }
